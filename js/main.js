bb8947f03f64daf4f114424dd73799c51a1de06c
Steven Kornblum <sdk1221@gmail.com>
2015-12-14 15:00:37 -0800

    cleanup

--- js/main.js
+++ js/main.js
@@ -1,207 +1,184 @@
 $(function () {
-    "use strict";
+
+  "use strict";
 
 
-    /* ==========================================================================
-   onscroll animation
-   ========================================================================== */
-		
-		if ($(window).width() > 992) {
-			$(window).fadeThis({
-					'reverse': false
-			});
-		};
-		
-    /* ==========================================================================
-   features slider
-   ========================================================================== */
+  /* ==========================================================================
+  onscroll animation
+  ========================================================================== */
 
-    $('.features-slider').slick({
-        dots: true,
-        arrows: false,
-				infinite: false
+  if ($(window).width() > 992) {
+    $(window).fadeThis({
+      'reverse': false
     });
+  };
 
 
-    /* ==========================================================================
-   tool tip
-   ========================================================================== */
+  /* ==========================================================================
+  features slider
+  ========================================================================== */
 
-    $('.nav a').tooltip();
+  $('.features-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: false
+  });
 
-    /* ==========================================================================
-   Team-slider
-   ========================================================================== */
+
+  /* ==========================================================================
+  tool tip
+  ========================================================================== */
+
+  $('.nav a').tooltip();
 
 
-    $('.team-slider').slick({
-        dots: true,
-        arrows: false,
-        infinite: false,
-        speed: 300,
+  /* ==========================================================================
+  Team-slider
+  ========================================================================== */
+
+  $('.team-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 3,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
         slidesToShow: 2,
-        slidesToScroll: 3,
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: true,
-                arrows: false
-            }
-        }, {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: true,
-                arrows: false
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: true,
-                arrows: false
-            }
-        }]
-    });
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+        arrows: false
+      }
+    }, {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false
+      }
+    }]
+  });
 
-    /* ==========================================================================
-   sub form
-   ========================================================================== */
 
-    var $form = $('#mc-form');
-    
-    $('#mc-subscribe').on('click', function(event) {
-        if (event)
-            event.preventDefault();
-        register($form);
-    });
-    
-    function register($form) {
-        $.ajax({
-            type: $form.attr('method'),
-            url: $form.attr('action'),
-            data: $form.serialize(),
-            cache: false,
-            dataType: 'json',
-            contentType: "application/json; charset=utf-8",
-            error: function(err) {
-                $('#mc-notification').hide().html('<span class="alert">Could not connect to server. Please try again later.</span>').fadeIn("slow");
-            
-            },
-            success: function(data) {
-                
-                if (data.result != "success") {
-                    var message = data.msg.substring(4);
-                    $('#mc-notification').hide().html('<span class="alert"><i class="fa fa-exclamation-triangle"></i>' + message + '</span>').fadeIn("slow");
-                
-                } else {
-                    var message = data.msg.substring(4);
-                    $('#mc-notification').hide().html('<span class="success"><i class="fa fa-envelope"></i>' + 'Awesome! We sent you a confirmation email.' + '</span>').fadeIn("slow");
-                
-                }
-            }
-        });
+  /* ==========================================================================
+  sub form
+  ========================================================================== */
+
+  var $form = $('#mc-form');
+
+  $('#mc-subscribe').on('click', function(event) {
+    if (event) {
+      event.preventDefault();
     }
+    register($form);
+  });
 
-
-    /* ==========================================================================
-   ScrollTop Button
-   ========================================================================== */
-
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.scroll-top a').fadeIn(200);
+  function register($form) {
+    $.ajax({
+      type: $form.attr('method'),
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      cache: false,
+      dataType: 'json',
+      contentType: "application/json; charset=utf-8",
+      error: function(err) {
+        $('#mc-notification').hide().html('<span class="alert">Could not connect to server. Please try again later.</span>').fadeIn("slow");
+      },
+      success: function(data) {
+        if (data.result != "success") {
+          var message = data.msg.substring(4);
+          $('#mc-notification').hide().html('<span class="alert"><i class="fa fa-exclamation-triangle"></i>' + message + '</span>').fadeIn("slow");
         } else {
-            $('.scroll-top a').fadeOut(200);
+          var message = data.msg.substring(4);
+          $('#mc-notification').hide().html('<span class="success"><i class="fa fa-envelope"></i>' + 'Awesome! We sent you a confirmation email.' + '</span>').fadeIn("slow");
         }
+      }
     });
+  }
 
 
-    $('.scroll-top a').click(function (event) {
-        event.preventDefault();
+  /* ==========================================================================
+  ScrollTop Button
+  ========================================================================== */
 
-        $('html, body').animate({
-            scrollTop: 0
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $('.scroll-top a').fadeIn(200);
+    } else {
+      $('.scroll-top a').fadeOut(200);
+    }
+  });
+
+  $('.scroll-top a').click(function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
+
+
+  /* ==========================================================================
+  Smooth Scroll
+  ========================================================================== */
+
+  $('a[href*=#]:not([href=#])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
         }, 1000);
+        return false;
+      }
+    }
+  });
+
+
+  /* ==========================================================================
+  Contact Form
+  ========================================================================== */
+
+  var form = $('#ajax-contact');
+  var formMessages = $('#form-messages');
+
+  $(form).submit(function (event) {
+    event.preventDefault();
+    var formData = $(form).serialize();
+    $.ajax({
+      type: 'POST',
+      url: $(form).attr('action'),
+      data: formData
+    }).done(function (response) {
+      $(formMessages).removeClass('error');
+      $(formMessages).addClass('success');
+      $(formMessages).hide().text(response).slideDown();
+      $('#name').val('');
+      $('#email').val('');
+      $('#message').val('');
+    }).fail(function (data) {
+      $(formMessages).removeClass('success');
+      $(formMessages).addClass('error');
+      if (data.responseText !== '') {
+        $(formMessages).text(data.responseText);
+      } else {
+        $(formMessages).hide().text('Oops! An error occured and your message could not be sent.').slideDown();
+      }
     });
-
-    /* ==========================================================================
-   Smooth Scroll
-   ========================================================================== */
-
-    $('a[href*=#]:not([href=#])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
-
-    /* ==========================================================================
-   Contact Form
-   ========================================================================== */
-
-
-    var form = $('#ajax-contact');
-
-
-    var formMessages = $('#form-messages');
-
-
-    $(form).submit(function (event) {
-
-        event.preventDefault();
-
-
-        var formData = $(form).serialize();
-
-
-
-
-        $.ajax({
-            type: 'POST',
-            url: $(form).attr('action'),
-            data: formData
-        }).done(function (response) {
-
-            $(formMessages).removeClass('error');
-            $(formMessages).addClass('success');
-
-
-            $(formMessages).hide().text(response).slideDown();
-
-
-            $('#name').val('');
-            $('#email').val('');
-            $('#message').val('');
-        }).fail(function (data) {
-
-            $(formMessages).removeClass('success');
-            $(formMessages).addClass('error');
-
-
-            if (data.responseText !== '') {
-                $(formMessages).text(data.responseText);
-            } else {
-                $(formMessages).hide().text('Oops! An error occured and your message could not be sent.').slideDown();
-            }
-        });
-
-
-
-
-    });
+  });
 
 
 });
+

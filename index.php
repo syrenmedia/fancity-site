0da963b5869c30ab5c28dd9b18e5e92bf03f9ccc
Steven Kornblum <sdk1221@gmail.com>
2015-12-17 11:54:25 -0800

    google analytics

--- index.php
+++ index.php
@@ -312,6 +312,7 @@
 
 
     <!--scripts-->
+    <script src="js/ga.min.js"></script>
     <script src="js/jquery-1.11.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.fadethis.min.js"></script>
